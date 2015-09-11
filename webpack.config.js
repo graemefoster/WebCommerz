a027a00ab2ade3666ba4ec43579518308404eaa2
Graeme Foster <graeme.foster@cashconverters.com>
2015-09-11 12:26:21 +0800

    Trying to get working in azure

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,11 @@ copyDirs.forEach(function (dir) {
   }
 });
 
+fs.writeFileSync('./dist/web.config', fs.readFileSync('./src/web.config'), {flag: 'w+'});
 fs.writeFileSync('./dist/index.html', fs.readFileSync('./src/index.html'), {flag: 'w+'});
 fs.writeFileSync('./dist/test-data/featured-items.json', fs.readFileSync('./src/test-data/featured-items.json'), {flag: 'w+'});
+
+fs.writeFileSync('./dev-server/web.config', fs.readFileSync('./src/web.config'), {flag: 'w+'});
 fs.writeFileSync('./dev-server/index.html', fs.readFileSync('./src/index.html'), {flag: 'w+'});
 fs.writeFileSync('./dev-server/test-data/featured-items.json', fs.readFileSync('./src/test-data/featured-items.json'), {flag: 'w+'});
 
