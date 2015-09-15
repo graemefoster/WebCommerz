5cb301c833fff1502196c90931c7f08169a7a421
graemefoster <graemefoster@gmail.com>
2015-09-15 21:02:06 +0800

    going mad with kudu

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var http = require('http')
+var react = require('react')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    res.end(react.rednerToStirng('Hello World\n');
 }).listen(port);
 
