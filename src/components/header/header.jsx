c6856ce9478f9b5e513f7dc96940c86826ce48e4
Graeme Foster <graeme.foster@cashconverters.com>
2015-09-15 09:24:43 +0800

    Bit more bootstrap

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,26 +3,45 @@ import Nav from 'react-bootstrap/lib/Nav';
 import CollapsibleNav from 'react-bootstrap/lib/CollapsibleNav';
 import Navbar from 'react-bootstrap/lib/Navbar';
 import NavItem from 'react-bootstrap/lib/NavItem';
+import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar';
+import Button from 'react-bootstrap/lib/Button';
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
+import Panel from 'react-bootstrap/lib/Panel';
+import Input from 'react-bootstrap/lib/Input';
 
 import './header.less';
 
 export default class Header extends React.Component {
 
-  constructor() {
-    super();
-  }
+    constructor() {
+        super();
+        this.state = {
+            open: false
+        };
+    }
 
     render() {
         return (
-          <Navbar brand="Webshop MOB" inverse toggleNavKey={0}>
-            <CollapsibleNav eventKey={0}>
-              <Nav navbar>
-                <NavItem eventKey={1} href={this.context.history.createHref('about')}>About</NavItem>
-                <NavItem eventKey={2} href={this.context.history.createHref('featured')}>Featured</NavItem>
-              </Nav>
-            </CollapsibleNav>
-          </Navbar>
-         );
+            <div>
+                <Navbar brand="Webshop MOB" inverse toggleNavKey={0}>
+                    <CollapsibleNav eventKey={0}>
+                        <Nav navbar>
+                            <NavItem eventKey={1} href={this.context.history.createHref('about')}>About</NavItem>
+                            <NavItem eventKey={2} href={this.context.history.createHref('featured')}>Featured</NavItem>
+                        </Nav>
+                    </CollapsibleNav>
+                    <ButtonToolbar>
+                        <Button><Glyphicon glyph="user"/></Button>
+                        <Button className="pull-right"
+                                onClick={ ()=> this.setState({ open: !this.state.open })}><Glyphicon
+                            glyph="search"/></Button>
+                    </ButtonToolbar>
+                </Navbar>
+                <Panel inverse collapsible="true" expanded={this.state.open}>
+                    <Input type="text" placeholder="Search..." />
+                </Panel>
+            </div>
+        );
     }
 }
 
