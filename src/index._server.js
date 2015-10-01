16f1715277f23af0874ba71670a26559720192fa
Graeme Foster <graeme.foster@cashconverters.com>
2015-10-01 14:55:45 +0800

    Added store into server-side render

--- src/index._server.js
+++ src/index._server.js
@@ -3,20 +3,17 @@ import { Provider } from 'react-redux';
 import { RoutingContext, match } from 'react-router'
 import routes from './routes/routes.jsx'
 import createLocation from 'history/lib/createLocation';
-//import configureStore from './store/configureStore';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 
 // declare our routes and their hierarchy
-const store = {
-    subscribe: function(){},
-    dispatch: function(){},
-    getState: function(){ return {items:[]}}
-};
-
 var serverRender = function(url, response) {
     console.log(url);
     let location = createLocation(url);
     var html;
     match({ routes, location}, (error, redirectLocation, renderProps) => {
+        var store = createStore(rootReducer);
+        console.log(store);
         var app = <div><Provider store={store}>{() => <RoutingContext {...renderProps} />}</Provider></div>;
         html = React.renderToString(app);
     });
